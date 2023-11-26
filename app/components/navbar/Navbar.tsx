726a37310516179523094b95301585868fea1ab0
HaoHanChen <kk12ll55@gmail.com>
2023-11-26 11:37:37 +0800

    feat: add category

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { Container } from '@/app/components/Container';
 import { Logo } from '@/app/components/navbar/Logo';
 import { Search } from '@/app/components/navbar/Search';
 import { UserMenu } from '@/app/components/navbar/UserMenu';
+import { Categories } from '@/app/components/navbar/Categories';
 
 interface NavbarProps {
     currentUser?: SafeUser | null;
@@ -22,6 +23,7 @@ export const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
     );
 };
