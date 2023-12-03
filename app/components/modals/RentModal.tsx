28fd981f18009201b5c4529ff3860c5433ad4c5d
HaoHanChen <kk12ll55@gmail.com>
2023-12-03 13:49:32 +0800

    feat: add steps description、price & finish submit function & add listings api

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -6,11 +6,15 @@ import { CategoryInput } from '@/app/components/inputs/CategoryInput';
 import { CountrySelect } from '@/app/components/inputs/CountrySelect';
 import { Counter } from '@/app/components/inputs/Counter';
 import { ImageUpload } from '@/app/components/inputs/ImageUpload';
+import { Input } from '@/app/components/inputs/Input';
 import { categories } from '@/app/components/navbar/Categories';
 import { Modal } from './Modal';
 import { Heading } from './Heading';
-import { FieldValues, useForm } from 'react-hook-form';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import dynamic from 'next/dynamic';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 enum STEPS {
     CATEGORY = 0,
@@ -23,8 +27,10 @@ enum STEPS {
 
 export const RentModal = () => {
     const rentModal = useRentModal();
+    const router = useRouter();
 
     const [step, setStep] = useState(STEPS.CATEGORY);
+    const [isLoading, setIsLoading] = useState(false);
     const {
         register,
         handleSubmit,
@@ -70,6 +76,26 @@ export const RentModal = () => {
 
     const onBack = () => setStep((prev) => prev - 1 || 0);
     const onNext = () => setStep((prev) => prev + 1);
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        if (step !== STEPS.PRICE) return onNext();
+
+        setIsLoading(true);
+        axios
+            .post('/api/listings', data)
+            .then(() => {
+                toast.success('Listing Created !');
+                router.refresh();
+                reset();
+                setStep(STEPS.CATEGORY);
+                rentModal.onClose();
+            })
+            .catch(() => {
+                toast.error('Something went wrong.');
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    };
 
     const actionLabel = step === STEPS.PRICE ? 'Create' : 'Next';
     const secondaryActionLabel = step === STEPS.CATEGORY ? undefined : 'Back';
@@ -169,6 +195,55 @@ export const RentModal = () => {
         );
     }
 
+    if (step === STEPS.DESCRIPTION) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title='How would you describe your place ?'
+                    subTitle='Short and sweet works best !'
+                />
+                <Input
+                    id='title'
+                    label='Title'
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+                <hr />
+                <Input
+                    id='description'
+                    label='Description'
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </div>
+        );
+    }
+
+    if (step === STEPS.PRICE) {
+        bodyContent = (
+            <div className='flex flex-col gap-8'>
+                <Heading
+                    title='Now, set your price'
+                    subTitle='How much do you charge per night ?'
+                />
+                <Input
+                    id='price'
+                    label='Price'
+                    formatPrice
+                    type='number'
+                    disabled={isLoading}
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </div>
+        );
+    }
+
     return (
         <Modal
             title='Airbnb your home !'
@@ -177,7 +252,7 @@ export const RentModal = () => {
             secondaryAction={onBack}
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
-            onSubmit={onNext}
+            onSubmit={handleSubmit(onSubmit)}
             body={bodyContent}
         />
     );
