b6d4c27d11c0703925dee2439fb279ee45c1366e
HaoHanChen <kk12ll55@gmail.com>
2023-12-10 11:40:02 +0800

    feat: add ListingCard and Favorite

---
+++ app/components/EmptyState.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Heading } from '@/app/components/modals/Heading';
+import { Button } from './Button';
+
+interface EmptyStateProps {
+    title?: string;
+    subTitle?: string;
+    showReset?: boolean;
+}
+
+export const EmptyState: React.FC<EmptyStateProps> = ({
+    title = 'No exact matches',
+    subTitle = 'Try changing or removing some of your filters',
+    showReset = false,
+}) => {
+    const router = useRouter();
+
+    return (
+        <div className='flex h-[60vh] flex-col items-center justify-center gap-2'>
+            <Heading
+                center
+                title={title}
+                subTitle={subTitle}
+            />
+            <div className='mt-4 w-48'>
+                {showReset && (
+                    <Button
+                        outline
+                        label='Remove all filters'
+                        onClick={() => router.push('/')}
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,47 @@
-export default function Home() {
-    return <div className='text-rose-500 text-2xl'>Hello Airbnb!</div>;
+import { Container } from '@/app/components/Container';
+import { EmptyState } from '@/app/components/EmptyState';
+import { ListingCard } from '@/app/components/listings/ListingCard';
+import { getCurrentUser } from '@/app/actions/getCurrentUser';
+import { getListings } from '@/app/actions/getListings';
+
+export default async function Home() {
+    const currentUser = await getCurrentUser();
+    const listings = await getListings();
+    const isEmpty = listings.length === 0;
+
+    if (isEmpty) {
+        return (
+            <div>
+                <EmptyState showReset />
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <Container>
+                <div
+                    className='grid 
+                    grid-cols-1 
+                    gap-8 
+                    pt-24 
+                    sm:grid-cols-2 
+                    md:grid-cols-3 
+                    lg:grid-cols-4 
+                    xl:grid-cols-5
+                    2xl:grid-cols-6'
+                >
+                    {listings.map((listing: any) => {
+                        return (
+                            <ListingCard
+                                key={listing.id}
+                                currentUser={currentUser}
+                                data={listing}
+                            />
+                        );
+                    })}
+                </div>
+            </Container>
+        </div>
+    );
 }
